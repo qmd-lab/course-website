37a5f86e0a1f84bea0773bd3580061f221e71b8d
andrewpbray <andrewbray@berkeley.edu>
2024-02-06 09:10:26 -0800

    add makeSchedule

--- prep-website.ts
+++ prep-website.ts
@@ -1,19 +1,23 @@
 
 // import qcw module
-import {readYML, unIgnoreFiles, makeListings } from "./qc-functions.ts"
+import {readYML, unIgnoreFiles, makeSchedule, makeListings } from "./qc-functions.ts"
 
 // set parameters
 const configPath = '_config.yml';
 const tempFilesDir = '';
 const quartoProfile = Deno.env.get("QUARTO_PROFILE");
+let renderType = "full";
+if (quartoProfile == "partial-site") {
+  renderType = "partial";
+}
 
-// load config and schedule files
+// load config
 const config = await readYML(configPath);
-const schedule = await readYML("schedule.yml");
 
 // pre-render steps
 if (quartoProfile == "partial-site") {
   await unIgnoreFiles(schedule);
 }
 
+const schedule = await makeSchedule(config, tempFilesDir, renderType);
 await makeListings(schedule, config, tempFilesDir);
