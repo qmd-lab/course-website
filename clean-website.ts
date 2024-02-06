5d84c72a5f4330ba05db11bc16b3e6e604592db5
andrewpbray <andrewbray@berkeley.edu>
2024-02-06 08:53:20 -0800

    pull functions out of script

--- clean-website.ts
+++ clean-website.ts
@@ -1,50 +1,21 @@
 
-import { parse } from "https://deno.land/std/yaml/mod.ts";
-import { join, dirname } from "https://deno.land/std/path/mod.ts";
+// import qcw module
+import { readYML, removeTempFiles } from "./qcw-functions.ts"
 
+// set parameters
 const configPath = '_config.yml';
 
-
-// ------------------------------------------- //
-//  Remove temp files generated during render  //
-// ------------------------------------------- //
-// This remove all -contents.yml files, schedule.yml, and sidebar-nav.yml
-// Is turned off if options: debug: true
-
-async function removeTempFiles(configPath: string) {
-    const yamlContent = await Deno.readTextFile(configPath);
-    const config = parse(yamlContent) as any;
-
-    const listingTypes = config['adaptive-nav']['listings'];
-
-    const filesToRemove: string[] = listingTypes.map((listing: any) => `${listing.type}-contents.yml`);
-    filesToRemove.push('sidebar-nav.yml', 'schedule.yml'); 
-
-    const scriptDir = dirname(new URL(import.meta.url).pathname);
-
-    for (const file of filesToRemove) {
-        const filePath = join(scriptDir, file);
-        try {
-            await Deno.remove(filePath);
-            console.log(`  - Removed file: ${filePath}`);
-        } catch (error) {
-            console.error(`  - Error removing file ${filePath}:`, error.message);
-        }
-    }
-}
-
-const yamlContent = await Deno.readTextFile(configPath);
-const config = parse(yamlContent) as any;
-
+// load config file
+const config = await readYML(configPath);
 const debugMode = config['options']?.debug || false;
 
-    
+// clean temp files if not in debug mode
 if (debugMode) {
   console.log("> Rendering complete!");
-  console.log("> Debug mode is on. Examine schedule.yml and associated nav yml files ...");
+  console.log("> Debug mode is on. Examine schedule.yml and associated nav yml files.");
 } else {
   console.log("> Cleaning up temporary files ...");
-  await removeTempFiles(configPath);
+  await removeTempFiles(config);
   console.log("> Rendering complete!");
 }
 
