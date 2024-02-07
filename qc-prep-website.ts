7c03970057fa11a0f96bc1bf77c16f851b377aa9
andrewpbray <andrewbray@berkeley.edu>
2024-02-07 11:53:53 -0800

    switch to ignoreFiles

--- qc-prep-website.ts
+++ qc-prep-website.ts
@@ -1,15 +1,11 @@
 
-// import qcw module
-import {readYML, unIgnoreFiles, makeSchedule, makeListings } from "./qc-functions.ts"
+// import qc module
+import {readYML, ignoreFiles, makeSchedule, makeListings } from "./qc-functions.ts"
 
 // set parameters
 const configPath = '_config.yml';
 const tempFilesDir = '';
-const quartoProfile = Deno.env.get("QUARTO_PROFILE");
-let renderType = "full";
-if (quartoProfile == "partial-site") {
-  renderType = "partial";
-}
+const renderType = Deno.env.get("QUARTO_PROFILE");
 
 // load config
 const config = await readYML(configPath);
@@ -17,8 +13,8 @@ const config = await readYML(configPath);
 // pre-render steps
 const schedule = await makeSchedule(config, tempFilesDir, renderType);
 
-if (quartoProfile == "partial-site") {
-  await unIgnoreFiles(schedule);
+if (renderType == "partial-site") {
+  await ignoreFiles(schedule, true);
 }
 
 await makeListings(schedule, config, tempFilesDir);
